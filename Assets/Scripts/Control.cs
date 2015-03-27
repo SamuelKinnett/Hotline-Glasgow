8f7d1ea9e0e4638b73600eeff9d87d79eb15724c
Samuel Kinnett <samuelkinnett@member.fsf.org>
2015-03-27 21:07:43 +0000

    Implemented Player Movement
    The player character now moves and rotates to face the mouse pointer.

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Control : MonoBehaviour {
 
+	private Animator animator;
+	public Camera camera;
+	public GameObject player;
+
+	bool isMoving;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = this.GetComponent<Animator>();
+		isMoving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		Vector3 playerPos = player.transform.position;
+		
+		playerPos.x += inputX / 4;
+		playerPos.y += inputY / 4;
+
+		//Credit to robertbu on Stack Overflow
+		Vector3 pos = camera.WorldToScreenPoint(transform.position);
+		Vector3 dir = Input.mousePosition - pos;
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+		if(inputX != 0 || inputY != 0)
+			animator.SetBool("Moving", true);
+		else
+			animator.SetBool("Moving", false);
+
+		player.transform.position = playerPos;
 	}
 }
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.position = player.transform.position;
+
+	}
+}
