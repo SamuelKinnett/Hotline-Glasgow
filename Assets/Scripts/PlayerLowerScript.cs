1410c306610d5d622fa544885d4df1b77a9ccd84
Samuel Kinnett <samuelkinnett@member.fsf.org>
2015-03-28 11:03:15 +0000

    Added Doors, Began Player Improvements
    Doors are now implemented and have physics, allowing them to be pushed
    open. In addition, the player now has separate upper body and leg
    objects, providing the framework for future attacking animations.

---
+++ Assets/Scripts/PlayerLowerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLowerScript : MonoBehaviour {
+
+	private Animator animator;
+
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position != player.transform.position)
+		{
+			this.transform.position = player.transform.position;
+			animator.SetBool("Moving", true);
+		}
+		else
+			animator.SetBool("Moving", false);
+	}
+}
---
+++ Assets/Scripts/SplashScreenPlaceholder.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenPlaceholder : MonoBehaviour {
+
+	public GameObject player;
+	bool fadingOut;
+
+	// Use this for initialization
+	void Start () {
+		fadingOut = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = player.transform.position;
+
+		if(Input.GetMouseButton(0))
+			fadingOut = true;
+
+		if (fadingOut);
+
+	}
+}
